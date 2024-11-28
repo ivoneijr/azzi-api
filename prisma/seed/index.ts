31918b3d4c0e760e9f515b3de2f7b8c79320c536
ivoneijr <ivoneijr@gmail.com>
2024-11-28 11:06:37 -0300

    remove seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,68 +1,12 @@
-import { Currency, ExpenseCategory, ExpenseStatus, PrismaClient, User, UserRole } from '@prisma/client'
-import { CONSTANTS } from './constants'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const createCategories = async (user: User): Promise<ExpenseCategory[]> => {
-  await prisma.expenseCategory.createMany({
-    data: [
-      { ...CONSTANTS.DUMMY.CATEGORIES.FIXED_COSTS, userId: user.id },
-      { ...CONSTANTS.DUMMY.CATEGORIES.COMFORT, userId: user.id },
-      { ...CONSTANTS.DUMMY.CATEGORIES.GOALS, userId: user.id },
-      { ...CONSTANTS.DUMMY.CATEGORIES.PLEASURES, userId: user.id },
-      { ...CONSTANTS.DUMMY.CATEGORIES.FINANCIAL_FREEDOM, userId: user.id },
-      { ...CONSTANTS.DUMMY.CATEGORIES.KNOWLEDGE, userId: user.id },
-    ],
-  })
-
-  const categories = await prisma.expenseCategory.findMany({
-    where: { userId: user.id }
-  })
-
-  return categories
+async function seed() {
+  // TODO: Implement seed
 }
 
-const createUser = async () => prisma.user.create({
-  data: {
-    email: 'ivoneijr@gmail.com',
-    password: CONSTANTS.DUMMY.PASSWORD,
-    role: UserRole.ADMIN,
-    profile: {
-      create: CONSTANTS.DUMMY.PROFILES.IVO
-    }
-  },
-})
-
-const createIncomes = async (user: User) => prisma.income.createMany({
-  data: [
-    { description: 'Salário', amount: 40000, userId: user.id, year: 2024, month: CONSTANTS.MONTHS.OCTOBER },
-  ],
-})
-
-const createExpenses = async (user: User, categories: ExpenseCategory[]) => prisma.expense.createMany({
-  data: [
-    {
-      ...CONSTANTS.DUMMY.EXPENSES.RENT,
-      status: ExpenseStatus.PAID,
-      userId: user.id,
-      categoryId: categories[0].id
-    },
-    {
-      ...CONSTANTS.DUMMY.EXPENSES.CONDOMINIUM,
-      userId: user.id,
-      categoryId: categories[0].id
-    },
-  ],
-})
-
-async function main() {
-  // const user = await createUser()
-  // const categories = await createCategories(user)
-  // await createIncomes(user)
-  // await createExpenses(user, categories)
-}
-
-main()
+seed()
   .then(async () => {
     await prisma.$disconnect()
   })
